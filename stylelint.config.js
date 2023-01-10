59dde4abadb14117a797071b133b58f26393c650
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2023-01-10 10:21:43 +0900

    check style lint

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,8 +1,22 @@
 module.exports = {
-  plugin: ["stylelint-scss"],
-  extends: [
-    "stylelint-config-recommended-scss", // scssのための拡張ルール追加
-    "stylelint-config-recess-order", // 視認性を考慮したcssプロパティの自動ソートを設定
-    "stylelint-config-prettier", // Prettierとの競合ルールをOFFにする
+  plugin: ['stylelint-scss', 'stylelint-order'],
+  ignoreFiles: [
+    // node_modulsディレクトリ配下にあるCSSを対象外する。
+    '**/node_modules/**',
   ],
+  extends: [
+    'stylelint-config-recommended-scss', // scssのための拡張ルール追加
+    'stylelint-config-recess-order', // 視認性を考慮したcssプロパティの自動ソートを設定
+    'stylelint-config-prettier', // Prettierとの競合ルールをOFFにする
+  ],
+  rules: {
+    'order/properties-alphabetical-order': true,
+    'at-rule-no-unknown': null,
+    'scss/at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: ['use', 'forward'],
+      },
+    ],
+  },
 };
